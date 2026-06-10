8f0902b0f315912850d3b01b5c32a723ba82f074
Darius <Reza.Blersch@gmail.com>
2026-06-10 13:22:56 +0200

    disabled LSP syntax highlighting in nvim

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -15,3 +15,11 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.textwidth = 80
   end,
 })
+
+-- disable LSP syntax coloring
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.semanticTokensProvider = nil
+  end,
+})
--- .config/nvim/lua/plugins/gruvboxmaterial.lua
+++ .config/nvim/lua/plugins/gruvboxmaterial.lua
@@ -10,15 +10,33 @@ return {
         local config = vim.fn["gruvbox_material#get_configuration"]()
         local p = vim.fn["gruvbox_material#get_palette"](config.background, config.foreground, config.colors_override)
         local hl = vim.fn["gruvbox_material#highlight"]
+
+        -- general config
+        hl("@constant", p.purple, p.none)
+        hl("@constant.builtin", p.purple, p.none)
+        hl("@operator", p.orange, p.none)
+        hl("@type", p.blue, p.none)
+        hl("@type.builtin", p.blue, p.none)
+        hl("@type.definition", p.blue, p.none)
+        hl("@variable.member", p.fg1, p.none)
+        hl("@variable", p.fg1, p.none)
+        hl("@property", p.fg1, p.none)
+        hl("@module", p.fg1, p.none)
+        hl("@namespace", p.fg1, p.none)
+        hl("@string", p.yellow, p.none)
+
         -- Rust specific config
+        hl("@keyword.operator.rust", p.red, p.none)
+        hl("@constant.rust", p.fg1, p.none)
+        hl("@constant.builtin.rust", p.fg1, p.none)
+        hl("@function.macro.rust", p.aqua, p.none)
+
+        -- LSP color config
         hl("@lsp.type.interface.rust", p.yellow, p.none)
         hl("@lsp.type.macro.rust", p.yellow, p.none)
         hl("@lsp.type.derive.rust", p.yellow, p.none)
         hl("@lsp.type.const.rust", p.purple, p.none)
         hl("@lsp.type.lifetime.rust", p.yellow, p.none)
-        hl("@keyword.operator.rust", p.red, p.none)
-        hl("@operator.rust", p.orange, p.none)
-        hl("@type", p.blue, p.none)
         vim.api.nvim_set_hl(0, "@lsp.type.struct.rust", { link = "@type" })
         vim.api.nvim_set_hl(0, "@lsp.type.enum.rust", { link = "@type" })
         vim.api.nvim_set_hl(0, "@lsp.type.builtinType.rust", { link = "@type" })
@@ -29,14 +47,19 @@ return {
         vim.api.nvim_set_hl(0, "@lsp.type.namespace.rust", { link = "@Fg" })
         vim.api.nvim_set_hl(0, "@lsp.type.property.rust", { link = "@Fg" })
         vim.api.nvim_set_hl(0, "@lsp.type.decorator.rust", { link = "@Fg" })
-        vim.api.nvim_set_hl(0, "@function.macro", { link = "@Fg" })
+        -- vim.api.nvim_set_hl(0, "@function.macro", { link = "@Fg" })
         vim.api.nvim_set_hl(0, "@punctuation.special.rust", { link = "@Fg" })
+
+        -- -- disable LSP highlighting
+        -- for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
+        --   vim.api.nvim_set_hl(0, group, {})
+        -- end
       end,
     })
 
     vim.g.gruvbox_material_disable_italic_comment = true
     vim.g.gruvbox_material_enable_italic = false
-    vim.g.gruvbox_material_background = "hard"
+    -- vim.g.gruvbox_material_background = "hard"
     vim.g.gruvbox_material_ui_contrast = "high"
     vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
     vim.g.gruvbox_material_diagnostic_line_highlight = true
