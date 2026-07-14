353a916b879b7f005883cf2e3e75fd504d7bd12f
Darius <Reza.Blersch@gmail.com>
2026-07-14 22:54:26 +0200

    config for sqlfluff formatting for postgres

--- .config/nvim/lua/plugins/fmt.lua
+++ .config/nvim/lua/plugins/fmt.lua
@@ -1,11 +1,14 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      markdown = { "prettier" },
-      python = { "ruff" },
-      bash = { "shfmt" },
-      sh = { "shfmt" },
-    },
-  },
+  opts = function(_, opts)
+    opts.formatters.sqlfluff = {
+      args = { "format", "--dialect=postgres", "-" },
+    }
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+    opts.formatters_by_ft.markdown = { "prettier" }
+    opts.formatters_by_ft.python = { "ruff" }
+    opts.formatters_by_ft.bash = { "shfmt" }
+    opts.formatters_by_ft.sh = { "shfmt" }
+    opts.formatters_by_ft.sql = { "sqlfluff" }
+  end,
 }
